b3aed6b2096ff04366aff816fbe252285f6958da
Paalis <pls19961219@gmail.com>
2021-04-25 20:42:34 -0400

    meta_learning

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ import matplotlib.pyplot as plt
 from model import BaselineModel
 from metrics import *
 import tqdm
+import copy
 
 import numpy as np
 from matplotlib import animation, rc
 from IPython.display import HTML, Image # For GIF
+
+torch.autograd.set_detect_anomaly(True)
 
 def plot_2D(position, orientation, obs_time=None, title_name=None):
     n_agent = position.shape[1]
@@ -83,10 +86,13 @@ def topk_neighbor(s, k=3):
     return s, s_neigh
 
 def train_model(EPOCHS, LR, device, model, train_loader, val_loader, vis_init):
+    # Meta Learning
+    meta_train = True
     # init setting
     log_freq = 10
     # initial the optimizer
-    optimizer = optim.Adam(model.parameters(), lr=LR)
+    # optimizer = optim.Adam(model.parameters(), lr=LR)
+    # optimizer_meta = optimizer(model)
     # initial running loss
     running_train_loss = 0
     running_val_loss = 0
@@ -95,7 +101,14 @@ def train_model(EPOCHS, LR, device, model, train_loader, val_loader, vis_init):
     val_loss_arr = []
 
     for epoch_index in tqdm.tqdm(range(EPOCHS)):
+        # if meta_train:
+        model_epoch = copy.deepcopy(model)
+        meta_grad = []
+        meta_optimizer = optim.Adam(model_epoch.parameters(), lr=LR)
         for iter_index, data in enumerate(train_loader, 0):
+            # if meta_train:
+            model = copy.deepcopy(model_epoch)
+            optimizer = optim.Adam(model.parameters(), lr=LR)
             data = data.to(device)
             bs, t, n_agent, s_dim = data.shape
             forward_t = t-1
@@ -117,12 +130,57 @@ def train_model(EPOCHS, LR, device, model, train_loader, val_loader, vis_init):
             # compute loss function
             loss = F.mse_loss(pred_delta, y_delta)
             optimizer.zero_grad()
-            loss.backward()  # backpropogation
+            loss.backward(retain_graph=True)  # backpropogation
             optimizer.step()
             # record train loss
             train_loss_arr.append(loss.item())
             # add up run loss for each batch
             running_train_loss += loss.item()
+
+            ## meta sub-train
+            if meta_train:
+                # data = data.to(device)
+                # bs, t, n_agent, s_dim = data.shape
+                # forward_t = t - 1
+                # x = data[:, :-1]
+                # y = data[:, 1:]
+                # y_delta = y - x
+                # init 0 pred
+                pred_delta = torch.zeros(bs, t - 1, n_agent, s_dim).to(device)
+                # init the hidden state
+                h = model.initHidden(n_agent, bs)
+                s = x[:, 0].reshape(-1, s_dim).unsqueeze(0)  # intput the first state into model
+                for t_index in range(forward_t):
+                    s, s_neighbor = topk_neighbor(s)
+                    s_input = torch.cat([s.unsqueeze(-2), s_neighbor], dim=2).flatten(start_dim=2, end_dim=3)
+                    s_residual, h = model(s_input, h)
+                    s = s + s_residual
+                    pred_delta[:, t_index] = s_residual.squeeze(0).reshape(bs, n_agent, s_dim)
+
+                # compute loss function
+                optimizer.zero_grad()
+                loss = F.mse_loss(pred_delta, y_delta)
+                loss.backward()
+                meta_grad.append(model.parameters())
+        ## Meta update on each epoch
+        if meta_train:
+            # gradient list for all
+            # add up the gradient
+            for m_index, m_para in enumerate(meta_grad):
+                if m_index == 0:
+                    grad_list = []
+                    for m_index, m in enumerate(m_para):
+                        temp = m.grad.data
+                        grad_list.append(temp)
+                        if m_index == 19:
+                            print(temp)
+                else:
+                    for m_index, m in enumerate(m_para):
+                        temp = m.grad.data
+                        grad_list[m_index] += temp
+                        if m_index == 19:
+                            print(temp)
+            print()
 
         # log
         if epoch_index % log_freq == 0:
