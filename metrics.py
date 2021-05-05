41a4ccb8cc32248be4a5ec529f38cc71c1f3ddd5
Paalis <pls19961219@gmail.com>
2021-05-04 21:12:01 -0400

    add position metric

---
+++ metrics.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import savgol_filter
+
+
+def position_consensus(position):
+    fig = plt.figure()
+    fig.set_size_inches(10, 5)
+
+
+    pos_diff_matrix = np.sqrt(np.sum((np.expand_dims(position, axis=-2) - np.expand_dims(position, axis=-3)) ** 2, axis=-1))
+    pos_consensus = np.sum(pos_diff_matrix, axis=(1, 2)) / 2
+    plt.plot(pos_consensus, label='pos_consensus')
+    plt.title('Position Consensus')
+    plt.xlabel('Time')
+    plt.ylabel('Position Vector Difference Norm Sum')
+    # plt.xlim([0, 400])
+    # plt.ylim([125, 280])
+    plt.legend()
+    print('mean position consensus of last 20 time stamps', np.mean(pos_consensus[-20:]))
+    plt.show()
